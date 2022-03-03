ec1fe731e9568692abbf4011fff11a66d0979820
= <ITPS@gmail.com>
2022-03-03 12:43:59 +0900

    자바 클래스 수업(수정)

--- JavaStudy_3714/src/a09_클래스/CarTest.java
+++ JavaStudy_3714/src/a09_클래스/CarTest.java
@@ -31,6 +31,12 @@ public class CarTest {
 	
 	public static void main(String[] args) {
 		Car c1 = new Car();
+		Car c3; //변수 선언(Car 참조 자료형)
+		c3 =  Car(); //Car 객체 생성 -> 힙메모리 영역에 Car클래스 사이즈 만큼 메모리를 빌려 할당하는 것
+		//10002588
+		String str = "김재현"; // 주소값 리터럴(상수)
+		
+		
 		c1.company = "KIA";
 		c1.model = "k5";
 		c1.oil = 5;
@@ -64,6 +70,11 @@ public class CarTest {
 		c1.oiling();
 		c1.showCarInfo();
 
+	}
+
+	private static Car Car() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
